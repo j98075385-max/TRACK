e4ad58f14d148345e39f8804a3309f59b33e5636
j98075385-max <j98075385@gmail.com>
2026-07-17 00:39:09 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,9 @@
+const CACHE = "habit-board-v1";
 
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
