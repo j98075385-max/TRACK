77a61142a8043dcd9d2640f8064b53fec14d2acc
j98075385-max <j98075385@gmail.com>
2026-07-17 00:35:31 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import HabitBoard from "./HabitBoard.jsx";
 
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HabitBoard />
+  </React.StrictMode>
+);
